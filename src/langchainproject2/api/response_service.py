2dd83174578bc6776d893a9ad18e63515d56215f
dhanashreemhatre <mhatredhanashree682@gmail.com>
2025-01-05 17:40:41 +0530

    multiple file upload

--- src/langchainproject2/api/response_service.py
+++ src/langchainproject2/api/response_service.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Body
 
 from ..chains.rag_chain import create_rag_chain
 from ..models.llm import create_llm
@@ -9,13 +9,16 @@ from ..utils.vector_store import get_vector_store
 router = APIRouter()
 
 
-@router.get("/output/{question}")
-async def give_output(question: str):
-    vectorstore = await get_vector_store()
-    llm = await create_llm()
-    retriever = await create_retriever(vectorstore)
-    prompt = await load_prompt()
-    rag_chain = await create_rag_chain(retriever, prompt, llm)
-    rag_output = rag_chain.invoke(question)
-    print(rag_output)
-    return {"output": rag_output}
+@router.post("/ask/{session_id}")
+async def give_output(session_id:str,question: str=Body(...)):
+    try:
+        vectorstore = await get_vector_store(collection_name=session_id)
+        llm = await create_llm()
+        retriever = await create_retriever(vectorstore)
+        prompt = await load_prompt()
+        rag_chain = await create_rag_chain(retriever, prompt, llm)
+        rag_output = rag_chain.invoke(question)
+        print(rag_output)
+        return {"output": rag_output}
+    except Exception as e:
+        return {"error": str(e)}
--- src/langchainproject2/utils/pdf_loader.py
+++ src/langchainproject2/utils/pdf_loader.py
@@ -1,15 +1,21 @@
 from langchain_community.document_loaders import PyPDFLoader
-from pdfplumber import open
-
+from io import BytesIO
+from pdfplumber import PDF
+from fastapi import UploadFile, HTTPException
 
 async def load_pdf(file_path):
     return PyPDFLoader(file_path)
 
 
-async def load_text(file_path):
-    file = open(file_path)
-    pages = file.pages
-    text = ""
-    for page in pages:
-        text += page.extract_text()
-    return text
+# Function to extract text from a single PDF
+async def load_text(file: UploadFile) -> str:
+    # Read file content into memory buffer
+    file_content = await file.read()
+    try:
+        with PDF(BytesIO(file_content)) as pdf:
+            text = ""
+            for page in pdf.pages:
+                text += page.extract_text() or ""
+            return text
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error processing file {file.filename}: {str(e)}")
